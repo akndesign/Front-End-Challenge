bf8a84cc8bf04ca2d152320c547ee5c37103dc8a
akndesign <alexanderkoziol@hotmail.com>
2022-05-21 23:00:08 -0700

    Migrate from create-react-app-buildpack to Node server

---
+++ react-ui/build/precache-manifest.1a00d4fb752cd55b643dcb2288734420.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a8f63a3163f1802f21a",
+    "url": "/static/css/main.7607ac87.chunk.css"
+  },
+  {
+    "revision": "4a8f63a3163f1802f21a",
+    "url": "/static/js/main.4a8f63a3.chunk.js"
+  },
+  {
+    "revision": "d11720e887a1448ed361",
+    "url": "/static/js/1.d11720e8.chunk.js"
+  },
+  {
+    "revision": "aba8b7eb49e1e61c1576",
+    "url": "/static/js/2.aba8b7eb.chunk.js"
+  },
+  {
+    "revision": "28dc535bcaba80dd7765",
+    "url": "/static/js/runtime~main.28dc535b.js"
+  },
+  {
+    "revision": "0f8b09aa0d84b1c63b0049b78712e7c0",
+    "url": "/static/media/send-02.0f8b09aa.svg"
+  },
+  {
+    "revision": "08e9a7f5753669adcbeed77fb724b8a0",
+    "url": "/index.html"
+  }
+];
