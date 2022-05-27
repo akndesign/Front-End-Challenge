d8fa018bc73a830de5fd2d33b84454aad1fc4660
akndesign <alexanderkoziol@hotmail.com>
2022-05-26 22:42:31 -0700

    Update to href link hover styling
    Change from default to smooth underline

--- src/components/Form/AlexanderNeumann.jsx
+++ src/components/Form/AlexanderNeumann.jsx
@@ -4,7 +4,7 @@ import "./Form.css";
 const AlexanderNeumann = () => {
     return (
         <span> Alexander Neumann is a 27-year-old graphic designer and developer of this front-end challenge.
-            In his role as a graphic designer, he has worked for <a className="hover-1" href="https://en.wikipedia.org/wiki/Christopher_Wylie"
+            In his role as a graphic designer, he has worked for <a className="hover8" href="https://en.wikipedia.org/wiki/Christopher_Wylie"
                 target="_blank" rel="noopener noreferrer">Christopher Wylie</a>, a UK travel app, < a href="https://www.instagram.com/p/Bbd_DdoDdny/" target="_blank" rel="noopener noreferrer">Citymapper</a>, as well as developed his own block stacking game, <a
                     href="https://vimeo.com/648414048" target="_blank" rel="noopener noreferrer">Misfits</a>. In a front-end capacity,
             he worked for <a href="https://edaid.com/"
